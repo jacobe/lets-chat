0b360c6ce919c9ccce0e6da808b8053efcbd46c2
sahas-arora <sahasarora@gmail.com>
2022-04-29 11:01:48 +0200

    Client added

---
+++ client.py
@@ -0,0 +1,18 @@
+import logging
+import grpc
+import chat_pb2
+import chat_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = chat_pb2_grpc.ChatStub(channel)
+        name = input("Enter your name")
+        password = input("Enter your password")
+        response = stub.Login(chat_pb2.LoginRequest(name=name, password=password))
+        print("Login request received: " + response)
+
+
+if __name__ == "main":
+    logging.basicConfig()
+    run()
