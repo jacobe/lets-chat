6f889e18cbffbc6115972f16116362ad84fdb03b
Przemek <przemek.maranowski@gmail.com>
2022-04-29 10:46:23 +0200

    Add server

---
+++ server.py
@@ -0,0 +1,46 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
+from concurrent import futures
+import logging
+import uuid
+
+import grpc
+import chat_pb2
+import chat_pb2_grpc
+
+password = "123"
+tokens = dict()
+
+
+class Chat(chat_pb2_grpc.ChatServicer):
+    def Login(self, request, context):
+        token = uuid.uuid4()
+        name = request.name
+        tokens[token] = name
+        return chat_pb2.Login(message=token)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    chat_pb2_grpc.add_ChatServicer_to_server(Chat(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
